303072f9ca7af4a22c1e03cfdfb5a4349e81132b
Marcelino <marcelino.malmeidan@gmail.com>
2018-04-22 22:59:48 -0500

    commit

---
+++ src/class_example.cpp
@@ -0,0 +1,23 @@
+// Cpp libraries
+#include <ros/ros.h>
+
+// Service type (for toggling shield)
+#include "mg_msgs/SetQuadBool.h"
+
+int main(int argc, char** argv){
+	ros::init(argc, argv, "class_example");
+	ros::NodeHandle node("~");
+
+	// Create service client
+	ros::ServiceClient shield_client = 
+		node.serviceClient<mg_msgs::SetQuadBool>("/mediation_layer/set_quad_shield");
+	mg_msgs::SetQuadBool srv_msg;
+	srv_msg.request.set_bool = 1;
+	srv_msg.request.quad_name = "ron";
+	shield_client.call(srv_msg);
+
+	// ROS loop that starts callbacks/publishers
+	ros::spin();
+
+	return 0;
+}
