d51307f085bb5a76c0bc1af4d4972e5be913fa3d
Marcelino <marcelino.malmeidan@gmail.com>
2018-04-22 23:14:47 -0500

    change1

--- src/class_example.cpp
+++ src/class_example.cpp
@@ -16,7 +16,7 @@ int main(int argc, char** argv){
 	srv_msg.request.quad_name = "ron";
 	shield_client.call(srv_msg);
 
-	std::cout << "Changed the shield for Harry!" << std::endl;
+	std::cout << "Changed the shield for Ron!" << std::endl;
 
 	// ROS loop that starts callbacks/publishers
 	ros::spin();
